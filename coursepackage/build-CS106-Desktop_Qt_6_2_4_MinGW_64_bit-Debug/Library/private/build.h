ce4c8d09e8bed986e2103bb420acf04614a54262
Radbruch <64899462+Radbruch@users.noreply.github.com>
2022-07-20 13:33:24 +0800

    update

---
+++ coursepackage/build-CS106-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Mon Jun 6 15:30:52 2022"
+#define SPL_BUILD_USER ""
+
+#endif
